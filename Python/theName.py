35f821da6c43c7898a51d4caaf25cec60a40c837
u230408 <adnan.ibrahim.2023@mumail.ie>
2025-05-17 21:31:53 +0100

    Excerise2

---
+++ Python/theName.py
@@ -0,0 +1,10 @@
+"""
+Write a Java program that prints the name "John Doe" three times total. Twice on line 1, and once on line 2.
+
+Sample input	Sample output
+N/A	            John Doe John Doe
+                John Doe
+"""
+
+print("John Doe John Doe")
+print("John Doe")
