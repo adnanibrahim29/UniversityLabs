b4f2312707f08b03308e0708c18d791506aaffe2
u230408 <adnan.ibrahim.2023@mumail.ie>
2025-05-17 21:36:05 +0100

    excerise3

---
+++ Python/whatPrints.py
@@ -0,0 +1,14 @@
+"""
+What do the below statements print? 
+
+Write a Java program that contains all of the statements to find out. 
+
+Sample code
+print("5 + 6")
+print(5 + 6)
+print(5 + "6")
+"""
+
+print(str("5 + 6"))
+print(5 + 6)
+print(str(5) + str("6"))
