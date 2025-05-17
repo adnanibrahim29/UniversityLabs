48750a627bbe686f63fe12e6b3cfabbfb63cf33d
u230408 <adnan.ibrahim.2023@mumail.ie>
2025-05-17 21:39:49 +0100

    excercise4

---
+++ Python/createVariables.py
@@ -0,0 +1,23 @@
+"""
+Write a Python program that:
+1. Declares an integer, called num1, and sets its value to be 12.
+2. Declares a double, called num2, and sets its value to be 123.4.
+3. Declares a character variable, called letter, and sets its value to be 'c'.
+4. Prints each initialised variable on a new line.
+
+You must not use print statements to print out the 12, 123.4 or c, but use them to print out the variables using their names.
+
+Note: You must use the variable names and values given.
+
+Sample input	Sample output
+N/A	            12 
+                123.4 
+                c
+"""
+num1: int = 12
+num2: float = 123.4  
+letter: str = 'c'    
+
+print(num1)
+print(num2)
+print(letter)
