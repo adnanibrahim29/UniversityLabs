f538bdc4577f93529abb459f7cdd49cc1f55bbfb
u230408 <adnan.ibrahim.2023@mumail.ie>
2025-05-17 21:05:28 +0100

    Excersise1

---
+++ Python/helloWorld.py
@@ -0,0 +1,10 @@
+"""
+Write a java program which prints the message "Hello World" to the screen twice on seperate lines.
+
+Sample input	Sample output
+N/A	            Hello World  Hello World
+
+"""         
+
+print("Hello World!")
+print("Hello World!")
