e5e34abe633886017266453cdb6e7d3de71e58e5
u230408 <adnan.ibrahim.2023@mumail.ie>
2025-05-18 15:40:22 +0100

    divisors

---
+++ Python/divisors.py
@@ -0,0 +1,13 @@
+""" 
+Write a Java program that uses a for loop to print all of the divisors of a given number. An example can be been in the sample input / output. You should print your answer as a comma seperated list.
+
+Sample input	Sample output
+24	            1,2,3,4,6,8,12,24
+"""
+
+num : int = 24
+string = ""
+for i in range(1, num + 1):
+    if num % i == 0:
+        string += str(i) + ","
+print(string[:-1])
