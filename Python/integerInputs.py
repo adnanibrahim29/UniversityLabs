e8b84e3caa6ba3328b5e192eecaeafe3e7559064
u230408 <adnan.ibrahim.2023@mumail.ie>
2025-05-18 19:25:54 +0100

    integerInputs

---
+++ Python/integerInputs.py
@@ -0,0 +1,16 @@
+""" 
+Write a program that takes two integer inputs from the user and prints their sum and product to the screen.
+
+Sample input	Sample output
+12              Sum: 55
+43              Product: 516
+"""
+print("Enter first Number: ")
+num1 : int = int(input())
+
+print("Enter Second Number: ")
+num2 : int = int(input())
+
+print("Sum: " + str(num1 + num2))
+print("Product: " + str(num1 * num2))
+
