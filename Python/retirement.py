44a47e1b60a63410f13e9ba3fe88f016f16c581d
u230408 <adnan.ibrahim.2023@mumail.ie>
2025-05-18 12:34:45 +0100

    retirement

---
+++ Python/retirement.py
@@ -0,0 +1,14 @@
+"""
+Write a Java program that checks to see if the age of a person is greater than or equal to 66. If it is, print out that the person is retired; otherwise, print that they are not retired. Use an integer variable age to represent the age of the person.
+
+Sample inputs	Sample outputs
+67	            Person is retired
+24	            Person is not retired
+"""
+
+age : int = 66
+
+if age >= 66:
+    print("Person is retired")
+else:
+    print("Person is not retired")
