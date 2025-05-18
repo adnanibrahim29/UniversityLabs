0f1f4e709a27fdfe6200c017b59dcc89c00831e6
u230408 <adnan.ibrahim.2023@mumail.ie>
2025-05-18 12:30:04 +0100

    excercise6

---
+++ Python/modulus.py
@@ -0,0 +1,20 @@
+""" 
+Write a Java program that uses an int variable called num that stores a 4 digit number. 
+
+Your program should then break up this number using the modulus operator into its component digits and print the message exactly as given below. 
+
+Make sure to test your program with another 4 digit number, but 1981 should be used for your final answer.
+
+Sample input	Sample output
+N/A	            The digits in the number 1981 are:
+                1
+                9
+                8
+                1
+"""
+num : int = 1981
+print(f"The digits in the number {num} are: ")
+print(num // 1000)  
+print((num % 1000) // 100)  
+print((num % 100) // 10)  
+print(num % 10) 
