6aaf7b4b45a3f35b3304b49f084b29f03372f469
u230408 <adnan.ibrahim.2023@mumail.ie>
2025-05-18 19:01:49 +0100

    reverseString

---
+++ Python/reverseString.py
@@ -0,0 +1,15 @@
+""" 
+Write a Java program that will take a String and reverse it. To complete this question Declare a String called reverse and assign it the value "programming". Your program should then reverse this string using a loop.
+
+Sample Input
+    programming
+Sample Output
+    gnimmargorp
+"""
+
+string : str = "programming"
+reversed : str = ""
+
+for i in range(len(string) - 1, -1, -1):
+    reversed += string[i]
+print(reversed)
